4c3224892d88328e234d66ab941d1df8acf40634
Alva010 <kaungkhant1107@gmail.com>
2024-12-06 23:39:51 +0630

    new_ui

---
+++ chat.py
@@ -0,0 +1,67 @@
+import os
+from dotenv import load_dotenv
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+groq_api_key = os.environ['GROQ_API_KEY']
+# model = 'Mixtral-8x7b-32768'
+model_chatbot_name = 'llama3-8b-8192'
+# model = 'whisper-large-v3-turbo'
+
+plant_prompt_template = PromptTemplate(
+    input_variables=["input", "history"],
+    template=(
+        "You are a chatbot specialized in plants. "
+        "You answer questions only about plants, including their care, biology, and uses. "
+        "If asked about unrelated topics, respond with: 'I only discuss plants.'\n\n"
+        "Conversation history:\n{history}\n\n"
+        "User: {input}\nChatbot:"
+    )
+)
+
+conversational_memory_length = 10
+memory = ConversationBufferMemory(k=conversational_memory_length)
+
+
+groq_chat = ChatGroq(
+        groq_api_key = groq_api_key,
+        model_name = model_chatbot_name,
+    )
+
+conversation = ConversationChain(
+        llm = groq_chat,
+        memory = memory,
+        prompt=plant_prompt_template
+    )
+
+# Chatbot configuration
+def initialize_chatbot():
+    groq_api_key = os.environ['GROQ_API_KEY']
+    model_chatbot_name = 'llama3-8b-8192'
+
+    plant_prompt_template = PromptTemplate(
+        input_variables=["input", "history"],
+        template=(
+            "You are a chatbot specialized in plants. "
+            "You answer questions only about plants, including their care, biology, and uses. "
+            "If asked about unrelated topics, respond with: 'I only discuss plants.'\n\n"
+            "Conversation history:\n{history}\n\n"
+            "User: {input}\nChatbot:"
+        )
+    )
+
+    memory = ConversationBufferMemory(k=10)
+    groq_chat = ChatGroq(
+        groq_api_key=groq_api_key,
+        model_name=model_chatbot_name,
+    )
+
+    return ConversationChain(llm=groq_chat, memory=memory, prompt=plant_prompt_template)
+
+
+conversational_memory_length = 10
+memory = ConversationBufferMemory(k=conversational_memory_length)
--- main.py
+++ main.py
@@ -1,158 +1,58 @@
 import streamlit as st
-import numpy as np
-import os
-from streamlit_option_menu import option_menu
+from chat import initialize_chatbot
+from image_detection import model_predict
+from utils import load_css, plant_animation, image_detection_animation, chat_animation
 from streamlit_lottie import st_lottie
-import requests
-import pathlib
-from PIL import Image # type: ignore
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image # type: ignore
-import matplotlib.pyplot as plt
-from tensorflow.keras.applications import imagenet_utils # type: ignore
-import json
-from dotenv import load_dotenv
-from groq import Groq
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from streamlit_option_menu import option_menu
+from PIL import Image
+from chat import conversation
 
-load_dotenv()
+# Load external CSS
+load_css("style/styles.css")
 
-#
-# Image Detection Codes
-#
+# Hide streamlit style
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            fotter {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
-model = tf.keras.models.load_model("plantdisease_model.h5")
+chatbot = initialize_chatbot()
 
-classes_file = 'class_names.json'
-with open(classes_file, 'r') as data:
-    class_names = json.load(data)
-
-
-# the function
-def model_predict(img):
-    # Preprocess the image
-    img = img.resize((224, 224))
-    img_array = image.img_to_array(img)
-    img_array = img_array / 255.0
-    final_image = np.expand_dims(img_array, axis=0)
-
-    # Predict with the ResNet50 model
-    predictions = model.predict(final_image)
-    predicted_class_idx = np.argmax(predictions[0])
-    confidence = np.max(predictions[0])
-
-    # Map the predicted index to the class label
-    predicted_class = class_names[predicted_class_idx]
-    return predicted_class, confidence
-
-
-
-#
-# Chatbot codes
-#
-
-
-groq_api_key = os.environ['GROQ_API_KEY']
-# model = 'Mixtral-8x7b-32768'
-model_chatbot_name = 'llama3-8b-8192'
-# model = 'whisper-large-v3-turbo'
-
-plant_prompt_template = PromptTemplate(
-    input_variables=["input", "history"],
-    template=(
-        "You are a chatbot specialized in plants. "
-        "You answer questions only about plants, including their care, biology, and uses. "
-        "If asked about unrelated topics, respond with: 'I only discuss plants.'\n\n"
-        "Conversation history:\n{history}\n\n"
-        "User: {input}\nChatbot:"
-    )
-)
-
-conversational_memory_length = 10
-memory = ConversationBufferMemory(k=conversational_memory_length)
-
-
-groq_chat = ChatGroq(
-        groq_api_key = groq_api_key,
-        model_name = model_chatbot_name,
-    )
-
-conversation = ConversationChain(
-        llm = groq_chat,
-        memory = memory,
-        prompt=plant_prompt_template
-    )
-
-
-# Set the page layout
-st.set_page_config(layout="wide")
-
-# Function to load external CSS
-def load_css(file_path):
-    with open(file_path) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-# Load the external CSS
-css_path = pathlib.Path("style/styles.css")
-if css_path.exists():
-    load_css(css_path)
-
-def load_lottie(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-# Load Lottie animations
-chat_animation = load_lottie("https://lottie.host/e66fbbc2-b561-41cd-a6dd-f544a29bec7b/A4tqdvyArS.json")
-contact = load_lottie("https://lottie.host/e77ad952-c631-4b87-a7e5-14358de78320/U0AfD37neX.json")
-plant_animation = load_lottie("https://lottie.host/60ce0978-f932-491c-8e0f-4ddaee3951f6/NwZNzCET2n.json")
-image_detection_animation = load_lottie("https://lottie.host/e77ad952-c631-4b87-a7e5-14358de78320/U0AfD37neX.json")
-
-# Header
-with st.container():
-    col1, col2 = st.columns(2)
-    with col1:
-      st.title("Welcome to Leaf-It")
-      with col2:
-        st_lottie(plant_animation, height=200)
-st.write("---")
 
 # Option menu for navigation
+
 selected = option_menu(
-    menu_title= None,
-    options=['Home', 'Chat', 'Image Detection'],
-    icons=['house', 'chat', 'camera'],
-    orientation= 'vertical',
-    styles={"columns": {"background-color": "#F0F2F6"}},
+        menu_title= None,
+        options=['Home', 'Chat', 'Image Detection'],
+        icons=['house', 'chat', 'camera'],
+        default_index=0,
+        orientation= 'horizontal',
+
 )
 
 if selected == "Home":
-    st.title("Welcome to Our Project")
-    st.write("""
-        This project demonstrates a multi-functional web application using **Streamlit**.
-        - **Chat**: Engage in real-time text input functionality.
-        - **Image Detection**: Upload an image and see our AI-powered detection in action.
-    """)
-    st.write("""
-        Feel free to explore and interact with the features.
-        Our goal is to make complex technologies accessible and user-friendly.
-    """)
-    st.write("---")
+    if plant_animation:
+        st_lottie(plant_animation, height=200)
+    else:
+        st.error("Failed to load Lottie animation.")
 
+    st.title("Welcome to Leaf-It")
+    st.write("Explore the features of this app.")
 
+# Chat bot
 
-
-# Chat UI start here ------------------->
-if selected == 'Chat':
+elif selected == 'Chat':
     with st.container():
-        st_lottie(chat_animation, height=200)
-        st.write("""
-        ### What can I help with?
-        """)
+        if chat_animation:
+            st_lottie(chat_animation, height=200)
+        else:
+            st.error("Failed to load Lottie animation.")
+
+        st.title("Chat with the Plant Expert")
 
         if "messages" not in st.session_state:
             st.session_state.messages = []
@@ -161,42 +61,40 @@ if selected == 'Chat':
 
         with chat_placeholder:
             for message in st.session_state.messages:
-                with st.chat_message(message["role"]):
+                with st.chat_message(message["role"], avatar="🌿"):
                     st.markdown(message["content"])
 
-        prompt = st.chat_input("What is up?")
+        prompt = st.chat_input("How can I help with plants?")
 
         if prompt:
             # Add user message to chat history
             st.session_state.messages.append({"role": "user", "content": prompt})
             # Display user message in chat message container
             with chat_placeholder:
-                with st.chat_message("user"):
+                with st.chat_message("user", avatar="🌿"):
                     st.markdown(prompt)
 
                 response = conversation.run(prompt)
 
-                with st.chat_message("assistant"):
+                with st.chat_message("assistant", avatar="🤖"):
                     st.markdown(response)
 
             # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.session_state.messages.append({"role": "assistant", "content": response,})
 
-
-
-
-
-
-# image detection start here ------------->
-if selected == 'Image Detection':
+# Image Detection
+elif selected == 'Image Detection':
     with st.container():
-        st_lottie(contact, height=200)
+        if image_detection_animation:
+            st_lottie(image_detection_animation, height=200)
+        else:
+            st.error("Failed to load Lottie animation.")
+
         st.header("Image Detection")
-        st.write("Detect objects in your images using our AI-powered detection tool.")
 
         # Create a form for image upload and detection
         with st.form(key='image_detection_form'):
-            uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
+            uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png", "jfif"])
             submit_button = st.form_submit_button(label='Detect Objects')
 
         if submit_button:
@@ -208,14 +106,9 @@ if selected == 'Image Detection':
                 # Perform object detection
                 with st.spinner('Detecting objects...'):
                    class_name, confidence = model_predict(img)
-                   st.write(f"**Predicted Class:** {class_name}")
+                   st.header(f"**Predicted Class:** {class_name}")
+
                 #    st.write(f"**Confidence:** {confidence:.2f}")
 
             else:
-                st.warning("Please upload an image file.")
-
-
-
-
-# Optional: Footer
-st.markdown("<hr><p style='text-align: center; color: '>© 2024 Leaf-It by Simbolo's Farmers.</p>", unsafe_allow_html=True)
+                st.warning("Please upload an image file. Click the Browse files button!")
